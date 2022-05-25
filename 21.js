f884196a5f0759d7290994aa8fe8d4036eab782d
«ifUride» <ifuride@gmail.com>
2022-05-25 22:13:05 +0300

    add codewars link to file

--- 21.js
+++ 21.js
@@ -3,6 +3,7 @@
 //because it uses the letters A-Z at least once (case is irrelevant).
 //Given a string, detect whether or not it is a pangram. Return True if it is, False if not.
 //Ignore numbers and punctuation.
+//https://www.codewars.com/kata/545cedaa9943f7fe7b000048
 
 function isPangram(string){
     let newString = string.toLowerCase();
